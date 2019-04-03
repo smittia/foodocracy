62e6356d6303df8342e8fcf5cf7bb0c5ea94fb73
Nathan Phillips <nathanphillips@MacBook-Pro-5.local>
2019-04-03 15:59:28 +0100

    remove old code

--- frontend-react/src/containers/app/index.js
+++ frontend-react/src/containers/app/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Route, Link } from 'react-router-dom'
 import Home from '../home'
-import OldHome from '../home_old'
 import About from '../about'
 import New from '../new'
 import Vote from '../vote'
@@ -16,7 +15,6 @@ const App = () => (
       <Route exact path="/" component={Home} />
       <Route exact path="/about-us" component={About} />
       <Route exact path="/new" component={New} />
-      <Route exact path="/old" component={OldHome} />
       <Route exact path="/vote" component={Vote} />
 
     </main>
